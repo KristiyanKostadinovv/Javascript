a4013878879e12064d2441245a632edf53931a69
KristiyanKostadinovv <kristiyan.kostadinov@unicard-uk.com>
2022-03-26 13:31:32 +0200

    Assignment 4 DOM

---
+++ assignments/dom-assignment-start/assignment.js
@@ -0,0 +1,11 @@
+const task1 = document.getElementById("task-1");
+const task2 = document.querySelector("title");
+//const task3 = document.getElementById("h1");
+const task3 = document.querySelector("h1");
+
+
+task1.style.color = "white";
+task1.style.background = "black";
+task2.textContent = "SOLVED";
+task3.textContent = "SOLVED";
+
