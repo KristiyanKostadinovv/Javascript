df8ba6a66eab026ceafa9b19cf3334dbfb91e9f9
KristiyanKostadinovv <kristiyan.kostadinov@unicard-uk.com>
2022-03-26 13:32:58 +0200

    Assignment 4

--- assignments/dom-assignment-start/assignment.js
+++ assignments/dom-assignment-start/assignment.js
@@ -1,4 +1,5 @@
 const task1 = document.getElementById("task-1");
+//const task2 = document.getElementById("title");
 const task2 = document.querySelector("title");
 //const task3 = document.getElementById("h1");
 const task3 = document.querySelector("h1");
