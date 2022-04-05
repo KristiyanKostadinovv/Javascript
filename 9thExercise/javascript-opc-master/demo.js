8ab86c2e3e119646dc590dd98a24df87633b3614
KristiyanKostadinovv <kristiyan.kostadinov@unicard-uk.com>
2022-04-05 12:40:24 +0300

    Push try

--- 9thExercise/javascript-opc-master/demo.js
+++ 9thExercise/javascript-opc-master/demo.js
@@ -1,6 +1,5 @@
 'use strict';
 
-(function() {
 /*
   const person ={
     fName: "Kris",
@@ -33,16 +32,57 @@
   mergeObj();
 */
 
-  function info(fname,lname, age){
-    this.fname = fname;
-    this.lname = lname;
-    this.age = age;
-    this.checkAdult = () => {return this.age >=18};
+/* function info(fname,lname, age){
+   this.fname = fname;
+   this.lname = lname;
+   this.age = age;
+   this.checkAdult = () => {return this.age >=18};
 
+ }
+
+ const createInfo = new info("Gosho", "Goshov", 22);
+ console.log(createInfo.checkAdult());
+ display(createInfo);
+ */
+
+
+/*
+  const objectPerson = {
+    fName: "Kris",
+    lName: "Kostadinov",
+    age: 22
   }
+  for (let loop in objectPerson)
+  {
+    display(loop + ": " + objectPerson[loop]);
+    /!*loop = loop + objectPerson[loop];
+    display(loop);*!/
+  }*/
 
-  const createInfo = new info("Gosho", "Goshov", 22);
-  console.log(createInfo.checkAdult());
-  display(createInfo);
+class Person {
+    constructor(fname, lname, age) {
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+    }
 
-})();
+    get getfullName() {
+        return this.fname + " " + this.lname;
+    }
+
+    set setfullName(fullname){
+      let part = fullname.split(" ");
+      this.fname = part[0];
+      this.lname = part[1];
+    }
+
+}
+
+const p1 = new Person("Kris", "Kostadinov", 22);
+p1.setfullName = "Mitko Berbatov"
+display(p1.getfullName);
+/*
+const p1 = new Person("Kris", "Kostadinov", 22);
+display();
+const p2 = new Person("Ivan", "Petrov", 22);
+display(p2.fullName);*/
