dd99291888c40f67706d1fe67d751f6c3c35a9cf
KristiyanKostadinovv <kristiyan.kostadinov@unicard-uk.com>
2022-04-12 17:46:41 +0300

    TS intro

--- 8thExercise/Typescript/Intro.js
+++ 8thExercise/Typescript/Intro.js
@@ -1,5 +1,18 @@
+var addNum1 = document.getElementById("num1");
+var addNum2 = document.getElementById("num2");
+var btnAdd = document.querySelector("button");
+//when globally initialized (doesn't convert to int)
+/*const ch1 = +addNum1.value;
+const ch2 = +addNum2.value;*/
 function add(a, b) {
     return a + b;
 }
-var num = add(5, 2);
-console.log(num);
+function sum() {
+    var num1 = parseInt(addNum1.value);
+    var num2 = parseInt(addNum2.value);
+    var sum = add(num1, num2);
+    //  const sum2 = add(ch1,ch2);
+    console.log(sum);
+    // console.log(sum2);
+}
+btnAdd.addEventListener("click", sum);
--- 8thExercise/Typescript/Intro.ts
+++ 8thExercise/Typescript/Intro.ts
@@ -1,5 +1,23 @@
-function add (a:number,b:number){
-    return a+b;
+const addNum1 = document.getElementById("num1") as HTMLInputElement;
+const addNum2 = <HTMLInputElement>document.getElementById("num2");
+const btnAdd = document.querySelector("button");
+
+//when globally initialized (doesn't convert to int)
+/*const ch1 = +addNum1.value;
+const ch2 = +addNum2.value;*/
+
+function add(a: number, b: number) {
+    return a + b;
 }
-const num = add(5,2);
-console.log(num);
+
+function sum() {
+
+    const num1 = parseInt(addNum1.value);//when inserted a character after a num doesn't calc it
+    const num2 = parseInt(addNum2.value);
+    const sum = add(num1, num2);
+    //  const sum2 = add(ch1,ch2);
+    console.log(sum);
+    // console.log(sum2);
+}
+
+btnAdd.addEventListener("click", sum);
